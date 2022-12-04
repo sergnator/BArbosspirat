7717a44cec0ec72779f853b7e53be917167c0fbb
sergnator <86975413+sergnator@users.noreply.github.com>
2022-12-04 22:05:26 +0300

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace about_me_csharp
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
